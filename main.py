9c3d7e15f50db014b1f50e043f797b30cfab7e8d
alirashidAR <alirashid.38@yahoo.com>
2025-02-26 00:57:39 +0530

    fix: render.yaml

--- main.py
+++ main.py
@@ -55,4 +55,4 @@ async def generate_jobs(request: RecommendJobsRequest):
     return jobs
 
 if __name__ == "__main__":
-    uvicorn.run(app, host="127.0.0.1", port=8000)
+    uvicorn.run(app, host="0.0.0.0", port=8000)
